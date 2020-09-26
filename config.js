a3060423d9677270958ce7feaa7d1c9182942786
lazyasskid <71947718+lazyasskid@users.noreply.github.com>
2020-09-27 05:36:16 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+/*
+Each API call costs 5 units!
+Single key can refresh the wall every 45 seconds, while 9 keys are needed for a refresh rate 
+of 5 seconds!
+*/
+
+var APIKeys = ["AIzaSyDDqp0q5t3a9XUH9aZRx32mVn9Bvg3b14Y"];
+
+var channelcount = 10;
+
+//Default Name
+var chanName = "Unknown";
+
+//Background Color
+var bgColor = "rgba(255, 255, 255,)";
+
+//Default Image url 
+var imgUrl = "https://yt3.ggpht.com/a-/AAuE7mB98CJL1Ye38OXbGM8WMR8lJVJRV_kXU1utHA=s240-mo-c-c0xffffffff-rj-k-no";
+
+$('body').css("background",bgColor);
---
+++ input.php
@@ -0,0 +1,38 @@
+<?php
+
+$no_of_channels = 5;
+$myFile = "channels.json";
+$arr_data = array(); // create empty array
+
+try {
+  $id = $_GET['id']; // get id from the url
+	$jsondata = file_get_contents($myFile); // get data from existing json
+  $arr_data = json_decode($jsondata, true); // converts json data into array
+  
+  function searchForId($arr_data, $id) {
+    foreach ($arr_data as $value) {
+      if ($value == $id) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  $check = searchForId($arr_data, $id);
+  if ($check == false) {
+    if (count($arr_data) > $no_of_channels-1) {
+      array_shift($arr_data);
+    }
+    array_push($arr_data,$id);
+    $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
+    if (file_put_contents($myFile, $jsondata)) {
+      echo "Your channel has been added to the list."; // chatbot success response
+    }
+  }
+  else {
+    echo "Your channel is already in the list."; // chatbot fail response
+  }
+}
+catch (Exception $e) {
+  echo 'Caught exception: ',  $e->getMessage(), "\n";
+   }
